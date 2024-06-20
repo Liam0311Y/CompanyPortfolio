97af77573dd9942eed39fda54aae388116d607cd
Liam0311Y <renato19880311@outlook.com>
2024-06-20 01:02:44 -0700

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "FullStack Developer",
+    company: "Fiverr & Upwork (freelance)",
+    duration: "(3+ years)"
+  },
+  {
+    id: 2,
+    title: "Blockchain Developer",
+    company: "Upwork & Toptal",
+    duration: "(2+ years)"
+  },
+  {
+    id: 3,
+    title: 'Frontend Developer',
+    company: "Aennova Ltd.",
+    duration: "(1 year)"
+  },
+  {
+    id: 4,
+    title: 'Backend & Frontend Developer',
+    company: "Biosom Ltd.",
+    duration: "(2 years)"
+  },
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Renato Yang",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "TopDevGroup involves over 40 Software Developers which are talented in different technical fields like FullStack, Blockchain, UI/UX Design, Web-based Game, DB Management, etc. We are pursuing a long-term relationship with clients by focusing on 3R strategy: Right time, Right Service, Right quality.",
+  email: 'renato19880311@gmail.com',
+  phone: '+12012121962',
+  address: 'New Jersey, United States, West Street ',
+  github: 'https://github.com/Renato03110',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
